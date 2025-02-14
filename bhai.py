b02c0c087956b0978d29b554aada678dafa724c1
sklfjlskaf <sdafjksfdakldsfdsfdkfljsdklfjd@gmail.com>
2025-02-13 20:56:07 -0800

    Update bhai.py

--- bhai.py
+++ bhai.py
@@ -300,7 +300,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./megoxer {target} {port} {time} 9 900"
+                    full_command = f"./paradox {target} {port} {time}"
                     subprocess.run(full_command, shell=True)
 
                     response = "Chudai completed successfully."
